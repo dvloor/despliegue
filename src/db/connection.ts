9e2dab44ac905854f9d2bd7218d933448e6eb25e
dvloor <dloor1460@utm.edu.ec>
2025-01-20 01:34:03 -0500

    16th commit

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,12 +4,6 @@ import dotenv from 'dotenv';
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASS:', process.env.DB_PASS ? '******' : '');
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
-
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
