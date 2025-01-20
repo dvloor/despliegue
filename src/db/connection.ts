14833b07ccc33854a3aae454c8f5e7441e93e0dc
dvloor <dloor1460@utm.edu.ec>
2025-01-20 00:05:54 -0500

    6th commit

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,25 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('almacen', 'root', 'Tm%$9vFwYp@3', {
-    host: 'localhost',
-    dialect: 'mysql'
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASS as string,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql', 
+  }
+);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión establecida exitosamente.');
+  })
+  .catch((error) => {
+    console.error('No se pudo conectar a la base de datos:', error);
   });
 
-  export default sequelize;
+export default sequelize;
