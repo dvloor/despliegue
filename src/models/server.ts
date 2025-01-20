c61e691caf0cb554da7b5d11a79a7596cc02f828
dvloor <dloor1460@utm.edu.ec>
2025-01-20 00:26:50 -0500

    9th commit

--- src/models/server.ts
+++ src/models/server.ts
@@ -17,7 +17,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || '3001';
+        this.port = process.env.PORT || '3000'; // Usar la variable de entorno PORT
         this.middlewares();
         this.routes();
         this.dbConnect();
