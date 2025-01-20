43807918847275c63cd20dd42c6ab809e7c99349
dvloor <dloor1460@utm.edu.ec>
2025-01-20 13:23:12 -0500

    20th commit

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,10 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    name: process.env.DB_NAME || 'almacen',
-    PORT: parseInt(process.env.DB_PORT || '3306', 10),
+    host: process.env.host || 'localhost',
+    user: process.env.user || 'root',
+    password: process.env.password || '',
+    name: process.env.name || 'almacen',
+    PORT: parseInt(process.env.PORT || '3306', 10),
   },
 };
